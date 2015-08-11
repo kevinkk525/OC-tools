a22db4de1238c5699c84596e5d89481489eeb785
kevinkk525 <kk93@gmx.net>
2015-08-11 11:08:10 +0200

    update

--- tool-downloader.lua
+++ tool-downloader.lua
@@ -2,8 +2,8 @@
 local version="0.1b"
 local author="kevinkk525"
 ---
-local filesystem=require"filesystem"
-local component=require"component",
+
+local component=require"component"
 local args = { ... }
 
 if not component.isAvailable("internet") then
@@ -18,19 +18,20 @@ if not component.internet.isHttpEnabled() then
 end
 
 print("downloading GUI-API")
-wget -f https://github.com/kevinkk525/OC-GUI-API/raw/master/shapes_default.lua /lib/shapes_default.lua
-wget -f https://github.com/kevinkk525/OC-GUI-API/raw/master/GUI.lua /lib/GUI.lua
-wget -f https://github.com/kevinkk525/OC-GUI-API/raw/master/term_mod.lua /lib/term_mod.lua
-wget -f https://github.com/kevinkk525/OC-GUI-API/raw/master/tech_demo.lua /tech_demo.lua
+os.execute("wget -f https://github.com/kevinkk525/OC-GUI-API/raw/master/shapes_default.lua /lib/shapes_default.lua")
+os.execute("wget -f https://github.com/kevinkk525/OC-GUI-API/raw/master/GUI.lua /lib/GUI.lua")
+os.execute("wget -f https://github.com/kevinkk525/OC-GUI-API/raw/master/term_mod.lua /lib/term_mod.lua")
+os.execute("wget -f https://github.com/kevinkk525/OC-GUI-API/raw/master/tech_demo.lua /tech_demo.lua")
 
 print("\ndownloading request-handler and modem-handler")
-wget -f https://github.com/kevinkk525/OC-tools/raw/master/modem-handler.lua /lib/modem_handler.lua
-wget -f https://github.com/kevinkk525/OC-tools/raw/master/request-handler.lua /lib/req_handler.lua
+os.execute("wget -f https://github.com/kevinkk525/OC-tools/raw/master/modem-handler.lua /lib/modem_handler.lua")
+os.execute("wget -f https://github.com/kevinkk525/OC-tools/raw/master/request-handler.lua /lib/req_handler.lua")
 
 print("downloading additional content")
 for i=1,#args do
     local url="https://github.com/kevinkk525/OC-tools/raw/master/"..args[i]
-    wget -f url "lib"..args[i]
+    local command="wget -f "..url.." /lib/"..args[i]
+    os.execute(command)
 end
 
 print("download finished")
