860e7ab9bfabf094cd229543bc82715a017f6383
kevinkk525 <kevinkk525@users.noreply.github.com>
2016-07-16 21:40:42 +0200

    more robust code

--- ME_Statistic_Expanded.lua
+++ ME_Statistic_Expanded.lua
@@ -4,56 +4,32 @@ local version="0.8b"
 
 local component=require("component")
 local event = require("event")
-local fs = require("filesystem")
 local keyboard = require("keyboard")
-local shell = require("shell")
 local term = require("term")
-local text = require("text")
-local unicode = require("unicode")
-local sides = require("sides")
-local colors=require("colors")
-local serialization=require("serialization")
-local modem=component.modem
-if modem.isWireless() then 
-    modem.setStrength(400)
-end
+local internet=require("internet")
 gpu=component.gpu
 local running=true
-local stats_now=false
 local hours=0
 local mins=0
 local tickCnt=0
+
 local ME=component.me_controller
-local items_tmp=ME.getItemsInNetwork()
-local fluids_tmp=ME.getFluidsInNetwork()
+
+local items_tmp={}
+local fluids_tmp={}
+
 local fluids={}
 local items={}
 local difference={}
+
 local w,h = gpu.getResolution()
 local per_row=h-12
 local rows=math.floor(w/53)
-local table=require("table")
+
 local dict={}
 local input={}
 local output={}
 local differ={}
-
-i=1
-while i<(#items_tmp+1) do
-    items[items_tmp[i].label]=items_tmp[i].size
-    dict[i]=items_tmp[i].label
-    dict[items_tmp[i].label]=i
-    i=i+1
-end
-items_tmp={}
-i=1
-while i<(#fluids_tmp+1) do
-    items[fluids_tmp[i].name.."_fluid"]=fluids_tmp[i].amount
-    dict[#dict+1]=fluids_tmp[i].name.."_fluid"
-    dict[fluids_tmp[i].name.."_fluid"]=#dict
-    i=i+1
-end
-fluids_tmp={}
 
 
 term.clear()
@@ -123,6 +99,7 @@ end
 
 -------------------------------------------------------------------------------
 
+local first_run=true
 while running do
     tickCnt = tickCnt + 1
     if tickCnt == 60 then
@@ -137,7 +114,30 @@ while running do
     term.setCursor(1,1)
     print("ME_Stats_Expanded")
     
-    if running==true then
+    if running==true and component.isAvailable("me_controller")==true then
+        if first_run==true then
+            ME=component.me_controller
+            items_tmp=ME.getItemsInNetwork()
+            fluids_tmp=ME.getFluidsInNetwork()
+            i=1
+            while i<(#items_tmp+1) do
+                items[items_tmp[i].label]=items_tmp[i].size
+                dict[i]=items_tmp[i].label
+                dict[items_tmp[i].label]=i
+                i=i+1
+            end
+            items_tmp={}
+            i=1
+            while i<(#fluids_tmp+1) do
+                items[fluids_tmp[i].name.."_fluid"]=fluids_tmp[i].amount
+                dict[#dict+1]=fluids_tmp[i].name.."_fluid"
+                dict[fluids_tmp[i].name.."_fluid"]=#dict
+                i=i+1
+            end
+            fluids_tmp={}
+            first_run=false
+        end
+        ME=component.me_controller
         items_tmpp=ME.getItemsInNetwork()
         fluids_tmpp=ME.getFluidsInNetwork()
         i=1
@@ -160,67 +160,64 @@ while running do
             i=i+1
         end
         fluids_tmpp={}
-        items_tmp[1]="do it"
     end
-    if items_tmp[1]~=nil then
-        i=1
-        j=1
-        while i<#dict+1 do
-            if items[dict[i]]==nil then
-                items[dict[i]]=0
-            end
-            if items_tmp[dict[i]]==nil then
-                items_tmp[dict[i]]=0
-            end
-            if items[dict[i]]~= items_tmp[dict[i]] then
-                difference[j]={}
-                difference[j][1]=dict[i]
-                difference[j][2]=items_tmp[dict[i]]-items[dict[i]]
-                if input[dict[i]]==nil then
-                    input[dict[i]]=0
-                    output[dict[i]]=0
-                end
-                if difference[j][2]>0 then
-                    input[dict[i]]=input[dict[i]]+difference[j][2]
-                elseif difference[j][2]<0 then
-                    output[dict[i]]=output[dict[i]]+difference[j][2]
-                end
-                j=j+1
-            end
-            if input[dict[i]]~=nil then
-                if differ[dict[i]]==nil then
-                    differ[#differ+1]=dict[i]
-                    differ[dict[i]]=1
-                end
-            end
-            i=i+1
+    i=1
+    j=1
+    while i<#dict+1 do
+        if items[dict[i]]==nil then
+            items[dict[i]]=0
         end
-        -------
-        k=1
-        m=0
-        --term.clear()
-        while k<rows*per_row+1 and differ[k]~=nil do
-            l=1
-            n=m*w/rows
-            while l~=per_row+1 and differ[k]~=nil do
-                printXY(l+4,n+3, "+%s", SI(input[differ[k]]))
-                printXY(l+4,n+10, "%s", SI(output[differ[k]]))
-                printXY(l+4,n+17, "%s", differ[k])
-                l=l+1
-                k=k+1
+        if items_tmp[dict[i]]==nil then
+            items_tmp[dict[i]]=0
+        end
+        if items[dict[i]]~= items_tmp[dict[i]] then
+            difference[j]={}
+            difference[j][1]=dict[i]
+            difference[j][2]=items_tmp[dict[i]]-items[dict[i]]
+            if input[dict[i]]==nil then
+                input[dict[i]]=0
+                output[dict[i]]=0
             end
-            m=m+1
+            if difference[j][2]>0 then
+                input[dict[i]]=input[dict[i]]+difference[j][2]
+            elseif difference[j][2]<0 then
+                output[dict[i]]=output[dict[i]]+difference[j][2]
+            end
+            j=j+1
         end
-        if #differ>rows*per_row then
-            printXY(l+4,((m-1)*w/rows)+10, "+%d Elemente",#differ-rows*per_row)
+        if input[dict[i]]~=nil then
+            if differ[dict[i]]==nil then
+                differ[#differ+1]=dict[i]
+                differ[dict[i]]=1
+            end
         end
-        centerF(h-6, "Differences: %d",#differ)
-        ------
-        items=items_tmp
-        items_tmp={}
-        difference={}
+        i=i+1
     end
-    
+    -------
+    k=1
+    m=0
+    --term.clear()
+    while k<rows*per_row+1 and differ[k]~=nil do
+        l=1
+        n=m*w/rows
+        while l~=per_row+1 and differ[k]~=nil do
+            printXY(l+4,n+3, "+%s", SI(input[differ[k]]))
+            printXY(l+4,n+10, "%s", SI(output[differ[k]]))
+            printXY(l+4,n+17, "%s", differ[k])
+            l=l+1
+            k=k+1
+        end
+        m=m+1
+    end
+    if #differ>rows*per_row then
+        printXY(l+4,((m-1)*w/rows)+10, "+%d Elemente",#differ-rows*per_row)
+    end
+    centerF(h-6, "Differences: %d",#differ)
+    ------
+    items=items_tmp
+    items_tmp={}
+    difference={}
+
     
     
     
